dff0d8696382aeeab580e75590ac4741cb19ed89
basanovase <79385478+basanovase@users.noreply.github.com>
2021-07-31 15:39:51 +1200

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,5 @@
 from statsnz import statsnz
 
-region_example = region_example = statsnz("YOUR_API_KEY",172.323,-41.242).get_region()
+region_example = region_example = statsnz("YOUR_API_KEY",-41.242,172.323).get_region()
 
 print(region_example)
